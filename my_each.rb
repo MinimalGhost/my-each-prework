8634e93a3631a8a87014c9e1abc084061cee748e
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 00:23:40 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,12 @@
 collection = ['arel', 'jon', 'logan', 'spencer']
 
 def my_each(array)
-  if block_given?
     i = 0
 
     while i < array.length
       yield array[i]
       i = i + 1
     end
-  else
-    "This block should not run!"
-  end
 
     array
 end
