230eda375f4485387f901200e074d0833b7f971e
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 00:15:05 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,16 @@
 collection = ['arel', 'jon', 'logan', 'spencer']
 
 def my_each(array)
+  if block_given?
     i = 0
 
     while i < array.length
       yield array[i]
       i = i + 1
     end
+  else  
 
+  end
     array
 
 end
